3e1bdc6dbd37a32efc0f35a789369639cb7b9b74
Büşra Talih <busra.talihhh@gmail.com>
2022-10-03 23:59:30 +0300

    ekleme 2

--- src/j11_MethodCreation/C04_MethodCreation.java
+++ src/j11_MethodCreation/C04_MethodCreation.java
@@ -1,2 +1,24 @@
-package j11_MethodCreation;public class C04_MethodCreation {
+package j11_MethodCreation;
+
+public class C04_MethodCreation {
+    public static void main(String[] args) {
+
+        String name=" haluk";
+        System.out.println("car car" + name);
+        C04_MethodDepo.gecmeNotu(24); //depo classtan class name ile metod call ettik
+        C04_MethodDepo.topla(3,5); //8 diğer class
+
+        //farklı classtan metod call etmek için
+        // 1. method static>gökteki gunes gibi
+        //2. className.methodName() seklinde aynı classtaki metot dogrudan call edilir
+
+        selamVer(); //aynı classtan metotName ile call edildi
+
+
+    }//main sonu
+
+    public static void selamVer(){
+        System.out.println("agam selamlar devamke");
+    }
+
 }
--- src/j11_MethodCreation/C04_MethodDepo.java
+++ src/j11_MethodCreation/C04_MethodDepo.java
@@ -1,2 +1,38 @@
-package j11_MethodCreation;public class C04_MethodDepo {
+package j11_MethodCreation;
+
+public class C04_MethodDepo {
+
+    // main metod olmayacak > motor yok > malzeme tasıyan romörk
+
+
+    //HER CLASSTA MAİN METOD MECBURİYETİ YOKTUR belirli aksiyonlara özel class'lar tanımlanabilir
+
+    public static void gecmeNotu(int not){
+        if (not>=85){
+            System.out.println("agam tebrik çoook basarili :)");
+        } else if (not>=70) {
+            System.out.println("agam basarili");
+
+        } else if (not>=60) {
+            System.out.println("agam sadece gectin");
+
+        } else if (not>=45) {
+            System.out.println("agam ucuz yırttın :)");
+
+        }
+        else System.out.println("agam fena caktin");
+    }
+
+    public static void topla(int a, int b){
+        System.out.println((a+b));
+    }
+
+
+
+
+
+
+
+
+
 }
