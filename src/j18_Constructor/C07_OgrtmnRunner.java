851b81a9074d468ed6d592d5439e29c122c026a0
Büşra Talih <busra.talihhh@gmail.com>
2022-10-21 20:22:17 +0300

    21.10.22

---
+++ src/j18_Constructor/C07_OgrtmnRunner.java
@@ -0,0 +1,23 @@
+package j18_Constructor;
+
+public class C07_OgrtmnRunner {
+    public static <C07_Ogrtm> void main(String[] args) {
+
+        C07_Ogrtmn hcm=new C07_Ogrtmn("SevdeNur Öğretmenim :)",11);
+        System.out.println("hcm.isim = " + hcm.isim);//
+        System.out.println("hcm.kıdem = " + hcm.kıdem);//
+        hcm.kıdemHesapla(13);//
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
